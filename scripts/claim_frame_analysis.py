d5c86dc2afdf957d4c672a264fda7eb406dc4072
Joe Cummings <jrcummings27@gmail.com>
2021-10-22 12:12:08 -0400

    Add claim frame analysis script

---
+++ scripts/claim_frame_analysis.py
@@ -0,0 +1,86 @@
+import json
+
+import argparse
+from collections import defaultdict
+import csv
+
+
+def main():
+    p = argparse.ArgumentParser(description=__doc__)
+    p.add_argument('--input', help='JSON input file')
+    args = p.parse_args()
+
+    with open(args.input, "r") as handle:
+        claims = json.load(handle)
+    
+    # distributions
+    num_of_each_topic = defaultdict(int)
+    num_of_each_subtopic = defaultdict(int)
+    
+    for claim in claims:
+        num_of_each_topic[claim["topic"]] += 1
+        num_of_each_subtopic[claim["subtopic"]] += 1
+    
+    with open("topic_distribution.csv", "w+") as handle:
+        writer = csv.writer(handle)
+        for k, v in num_of_each_topic.items():
+            writer.writerow([k, v])
+    
+    with open("subtopic_distribution.csv", "w+") as handle:
+        writer = csv.writer(handle)
+        for k, v in num_of_each_subtopic.items():
+            writer.writerow([k, v])
+    
+    # claim detection accuracy
+    num_of_claims = len(claims)
+    print(f"Num of claims: {num_of_claims}")
+
+    # x variable & qnode accuracy
+    num_x_variable = 0
+    num_x_q_nodes = 0
+    for claim in claims:
+        if claim["x_variable"]:
+            num_x_variable += 1
+        if claim["x_variable_qnode"]:
+            num_x_q_nodes += 1
+    print(f"% X variables found: {num_x_variable / num_of_claims}")
+    print(f"% X variable qnodes found: {num_x_q_nodes / num_x_variable}")
+
+    # claimer & qnode accuracy
+    num_claimers = 0
+    num_claimer_q_nodes = 0
+    for claim in claims:
+        if claim["claimer"]:
+            num_claimers += 1
+        if claim["claimer_qnode"]:
+            num_claimer_q_nodes += 1
+    print(f"% of claimers found over total: {num_claimers / num_of_claims}")
+    print(f"% of claimer qnodes found: {num_claimer_q_nodes / num_claimers}")
+
+    # overall wikidata accuracy
+    wikidata_total = 0
+    wikidata_good = 0
+    for claim in claims:
+        print(claim["claim_text"])
+        if claim["x_variable_qnode"]:
+            good = int(input(f"{claim['x_variable_qnode']}\n"))
+            wikidata_good += good
+            wikidata_total += 1
+        if claim["claimer_qnode"]:
+            good = int(input(f'{claim["claimer_qnode"]}\n'))
+            wikidata_good += good
+            wikidata_total += 1
+        if claim["claim_semantics"]:
+            if claim["claim_semantics"]["event"]:
+                good = int(input(f'{claim["claim_semantics"]["event"]}\n'))
+                wikidata_good += good
+                wikidata_total += 1
+            if claim["claim_semantics"]["args"]:
+                for _, arg in claim["claim_semantics"]["args"].items():
+                    good = int(input(f'{arg}\n'))
+                    wikidata_good += good
+                    wikidata_total += 1
+    print(f"% of accurate qnode selections: {wikidata_good / wikidata_total}")
+
+if __name__ == '__main__':
+    main()
