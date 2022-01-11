c5d7f580de2f8948c8d9d01f33d7ecf3d88a4bf5
Liz Lee <elgclee@gmail.com>
2022-01-11 17:46:11 -0500

    Update EDL ingester for latest CS output

--- cdse_covid/pegasus_pipeline/ingesters/edl_output_ingester.py
+++ cdse_covid/pegasus_pipeline/ingesters/edl_output_ingester.py
@@ -14,7 +14,8 @@ class EDLEntity:
 
     ent_id: str
     ent_type: str
-    freebase_link: Optional[str] = None
+    ent_link: Optional[str] = None
+    type_link: Optional[str] = None
 
 
 @dataclass
@@ -34,7 +35,7 @@ def main(edl_output: Path, output: Path) -> None:
         dict
     )
     set_aside_mentions: List[Tuple[str, str, str, str, Tuple[int, int]]] = []
-    with open(edl_output / "merged.cs", "r", encoding="utf-8") as handle:
+    with open(edl_output, "r", encoding="utf-8") as handle:
         reader = csv.reader(handle, delimiter="\t")
         all_entities: Dict[str, EDLEntity] = {}
         # Collect all entities before looking for mentions
@@ -42,27 +43,37 @@ def main(edl_output: Path, output: Path) -> None:
             if len(line) == 3:  # We have ourselves an entity
                 _id = line[0].split("_")
                 formatted_id = _id[-1]
-                formatted_link = None
+                ent_link = None
+                type_link = None
                 ent_type = ""
 
-                if line[1] == "link" and line[2].startswith("m."):
-                    freebase_link = line[2].split("_")[0]
-                    formatted_link = "/" + freebase_link.replace(".", "/")
+                if line[1] == "link" and line[2].startswith("Q"):
+                    ent_link = line[2]
                     new_entity = EDLEntity(
-                        ent_id=formatted_id, ent_type=ent_type, freebase_link=formatted_link
+                        ent_id=formatted_id, ent_type=ent_type, ent_link=ent_link
+                    )
+                elif line[1] == "typelink" and line[2].startswith("Q"):
+                    type_link = line[2]
+                    new_entity = EDLEntity(
+                        ent_id=formatted_id, ent_type=ent_type, type_link=type_link
                     )
                 elif line[1] == "type":
-                    ent_type = line[2]
+                    ent_type_uri = line[2]
+                    # Type example: `https://tac.nist.gov/tracks/SM-KBP/2019/ontologies/LDCOntology#GPE.City`
+                    ent_type = ent_type_uri.split("#")[-1]
                     new_entity = EDLEntity(ent_id=formatted_id, ent_type=ent_type)
 
                 if all_entities.get(formatted_id):
                     # The entity already exists in our collection, so we just modify it
-                    if formatted_link:
-                        all_entities[formatted_id].freebase_link = formatted_link
+                    if ent_link:
+                        all_entities[formatted_id].ent_link = ent_link
+                    elif type_link:
+                        all_entities[formatted_id].type_link = type_link
                     elif ent_type:
                         all_entities[formatted_id].ent_type = ent_type
                 else:
                     all_entities[formatted_id] = new_entity
+
             elif len(line) == 5:  # Otherwise it's a mention
                 _ent_id = line[0].split("_")
                 formatted_ent_id = _ent_id[-1]
@@ -72,7 +83,7 @@ def main(edl_output: Path, output: Path) -> None:
                 doc = doc_and_span[0]
                 _span = doc_and_span[1].split("-")
                 formatted_span = (int(_span[0]), int(_span[1]))
-                # Entities for mentions do not have their freebase links yet.
+                # Entities for mentions do not have their Qnode links yet.
                 # Set aside the mention for examination
                 # until after all entity data is collected.
                 set_aside_mentions.append(
@@ -96,7 +107,7 @@ def main(edl_output: Path, output: Path) -> None:
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument("--edl-output", help="Path to EDL dir containing output", type=Path)
+    p.add_argument("--edl-output", help="Path to EDL output in ColdStart format", type=Path)
     p.add_argument("--output", help="Path to output file", type=Path)
     args = p.parse_args()
     main(args.edl_output, args.output)
